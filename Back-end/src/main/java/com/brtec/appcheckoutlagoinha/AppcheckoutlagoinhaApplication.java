e2faf61694bf567cd178d4c3a39848a0fe39570f
borg <bruno.riguetto@gft.com>
2023-02-06 10:26:59 -0300

    Iniciando projeto

---
+++ Back-end/src/main/java/com/brtec/appcheckoutlagoinha/AppcheckoutlagoinhaApplication.java
@@ -0,0 +1,13 @@
+package com.brtec.appcheckoutlagoinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcheckoutlagoinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcheckoutlagoinhaApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/com/brtec/appcheckoutlagoinha/AppcheckoutlagoinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brtec.appcheckoutlagoinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcheckoutlagoinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
